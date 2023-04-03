908421948d8efea551db49d445d97d72c03fe7ef
Grant <seregagranto@gmail.com>
2023-04-03 15:40:59 +0300

    rework content page

--- app/js/app.js
+++ app/js/app.js
@@ -225,3 +225,41 @@ class SelectWrapper {
 }
 
 const mySelectWrapper = new SelectWrapper("select");
+
+const swiper = new Swiper(".mySwiper", {
+	slidesPerView: 3,
+	spaceBetween: 20,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	}, breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 3,
+			spaceBetween: 10
+		},
+		// when window width is >= 480px
+		480: {
+			slidesPerView: 3,
+			spaceBetween: 10
+		},
+		// when window width is >= 640px
+		640: {
+			slidesPerView: 3,
+			spaceBetween: 20
+		},
+		1000: {
+			slidesPerView: 1,
+			spaceBetween: 10
+		}
+		,
+		1200: {
+			slidesPerView: 2,
+			spaceBetween: 10
+		}
+		, 1600: {
+			slidesPerView: 3,
+			spaceBetween: 20
+		}
+	}
+});
